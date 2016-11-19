7bf631a109155e5fa1b24c33a17da41759c20d75
Septian A Tama <sfirst.tama@gmail.com>
2016-11-19 17:48:06 +0700

    adding route for apiEo

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes/index');
-const users = require('./routes/users');
+const apiEo = require('./routes/apiEo');
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api/eo', apiEo);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
