4bc5628addd67c3a440115efe4c5d7a91ecf5dc1
Septian A Tama <sfirst.tama@gmail.com>
2016-11-19 17:50:49 +0700

    add eos model

---
+++ server/models/Eos.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const validate = require('mongoose-validator');
+
+var emailValidator = [
+  validate({
+    validator: 'isEmail',
+    message: 'Please provide a valid email address'
+  })
+]
+
+let EosSchema = new mongoose.Schema ({
+  eventName: {
+    type: String,
+    required: true
+  },
+  eventTitle: {
+    type: String,
+    required: true
+  },
+  email : {
+    type: String,
+    required: true,
+    validate: emailValidator
+  },
+  dateCreated: Date
+})
+
+module.exports = mongoose.model('Eos', EosSchema)
